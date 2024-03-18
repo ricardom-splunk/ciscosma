e5c03f6ab3f9b4a91a21929bbd6a14fe14f36839
Ricardo Moreira <ricardom@splunk.com>
2024-03-18 12:55:43 +0000

    feat: Added get, delete and release actions

--- ciscosma_consts.py
+++ ciscosma_consts.py
@@ -1,3 +1,3 @@
 GET_JWT_TOKEN = "/sma/api/v2.0/login"
-MESSAGES = "/sma/api/v2.0/messages"
-MESSAGES_DETAILS = "/sma/api/v2.0/messages/details"
+MESSAGES = "/sma/api/v2.0/quarantine/messages"
+MESSAGES_DETAILS = "/sma/api/v2.0/quarantine/messages/details"
